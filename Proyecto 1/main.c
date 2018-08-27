b758ad94b896d80c2b8b8a76dbdb0a3841d5a0b8
rosasthomas <rosasthomas2@gmail.com>
2018-08-27 11:54:59 -0300

    Agregue logica

--- Proyecto 1/main.c
+++ Proyecto 1/main.c
@@ -6,13 +6,42 @@ int main()
     int numeroUno;
     int numeroDos;
     int numeroTres;
+    int max;
+    int min;
+    int medio;
 
     printf("Ingrese el primero numero: ");
     scanf("%d", &numeroUno);
+    max = numeroUno;
+    min = numeroUno;
+
     printf("Ingrese el segundo numero: ");
     scanf("%d", &numeroDos);
+    if(numeroDos < min){
+        min = numeroDos;
+    }else if(numeroDos > max){
+        max = numeroDos;
+        }
+
     printf("Ingrese el tercer numero: ");
     scanf("%d", &numeroTres);
+    if(numeroTres < min){
+        min = numeroTres;
+    }else if(numeroTres > max){
+        max  = numeroTres;
+    }
 
+    if(numeroUno > min && numeroUno < max){
+        medio = numeroUno;
+        printf("El numero del medio es = %d", numeroUno);
+    }else if(numeroDos > min && numeroDos < max){
+        medio = numeroDos;
+        printf("El numero del medio es = %d", numeroDos);
+    }else if(numeroTres > min && numeroTres < max){
+        medio = numeroTres;
+        printf("El numero del medio es = %d", numeroTres);
+    }else{
+        printf("No hay numero del medio");
+    }
     return 0;
 }
