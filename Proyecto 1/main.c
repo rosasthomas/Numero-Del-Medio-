e1dd28edc9d0fba0d44b50452b41e1e799eecbf6
alumno <alumno>
2018-08-27 11:30:01 -0300

    Ingreso de numeros

---
+++ Proyecto 1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+
+    printf("Ingrese el primero numero: ");
+    scanf("%d", &numeroUno);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &numeroDos);
+    printf("Ingrese el tercer numero: ");
+    scanf("%d", &numeroTres);
+
+    return 0;
+}
